0a44e9fbeb675bf56335cc6c6615122f51fd8987
Nobufumi Yoshida <nobufumi.yoshida@gmail.com>
2022-10-30 04:39:44 +0900

    set quest

--- pages/2e4d9825d6f2205615f5315f63f6586bd142e1f7b7de71f4030ce0ea99e786ec.tsx
+++ pages/2e4d9825d6f2205615f5315f63f6586bd142e1f7b7de71f4030ce0ea99e786ec.tsx
@@ -6,9 +6,17 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
+        <title>set001</title>
+        <meta name="description" content="set001" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://www.info-ono.jp/">Ono Quest</a>
+        </h1>
+
+        <Image src="/images/imagetest01.png"  alt="My avatar "width={320} height={132} />
       </main>
 
       <footer className={styles.footer}>
