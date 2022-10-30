6f61fad1ae3f2ae9a0982fcf1173db70810e58da
Nobufumi Yoshida <nobufumi.yoshida@gmail.com>
2022-10-30 12:30:47 +0900

    onoquest

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>set001</title>
+        <title>OnoQuest</title>
         <meta name="description" content="set001" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
